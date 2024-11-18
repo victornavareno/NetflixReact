084ad38f3df13f365590aa02e32414a5b647adda
victornavareno <vnavareo@alumnos.unex.es>
2024-11-18 18:00:20 +0100

    funciona todo (hay bugs de css creo)

--- src/components/ContenidoBox.jsx
+++ src/components/ContenidoBox.jsx
@@ -1,20 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/ContenidoBox.css";
 
 function ContenidoBox({ contenido }) {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate(`/contenido/${contenido.idcontenido}`, { state: { contenido } });
+  };
+
   return (
-    <div className="contenido-box">
-      <div className="contenido-image">
+    <div className="contenido-box" onClick={handleNavigate}>
+      <div className="contenido-box-image">
         {/* <img src={contenido.imagen || "placeholder.jpg"} alt={contenido.titulo} /> */}
         {/* <img src="/assets/img/FRIENDS.jpg" alt="Breaking Bad" /> */}
         {/* IMPORTANTE: HAY QUE ESTANDARIZAR EL NOMBRE DE 'IMAGEN' EN LA BASE DE DATOS */}
-        <img src={`/assets/img/${contenido.imagen}`} alt={`${contenido.imagen}`} />
-      
-
+        <img
+          src={`/assets/img/${contenido.imagen}`}
+          alt={`${contenido.imagen}`}
+        />
       </div>
-      <div className="contenido-info">
-        <h3 className="contenido-title">{contenido.titulo}</h3>
-        <p className="contenido-sinopsis">{contenido.sinopsis}</p>
+      <div className="contenido-box-info">
+        <h3 className="contenido-box-title">{contenido.titulo}</h3>
+        <p className="contenido-box-sinopsis">{contenido.sinopsis}</p>
       </div>
     </div>
   );
