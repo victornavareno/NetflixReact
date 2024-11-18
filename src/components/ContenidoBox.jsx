1ac4be2540e1084826154f7fa83ae5833e97b8c7
victornavareno <vnavareo@alumnos.unex.es>
2024-11-18 11:10:42 +0100

    IMPORTANTE: HAY QUE ESTANDARIZAR EL NOMBRE DE 'IMAGEN' EN LA BASE DE DATOS

--- src/components/ContenidoBox.jsx
+++ src/components/ContenidoBox.jsx
@@ -5,7 +5,12 @@ function ContenidoBox({ contenido }) {
   return (
     <div className="contenido-box">
       <div className="contenido-image">
-        <img src={contenido.imagen || "placeholder.jpg"} alt={contenido.titulo} />
+        {/* <img src={contenido.imagen || "placeholder.jpg"} alt={contenido.titulo} /> */}
+        {/* <img src="/assets/img/FRIENDS.jpg" alt="Breaking Bad" /> */}
+        {/* IMPORTANTE: HAY QUE ESTANDARIZAR EL NOMBRE DE 'IMAGEN' EN LA BASE DE DATOS */}
+        <img src={`/assets/img/${contenido.imagen}`} alt={`${contenido.imagen}`} />
+      
+
       </div>
       <div className="contenido-info">
         <h3 className="contenido-title">{contenido.titulo}</h3>
