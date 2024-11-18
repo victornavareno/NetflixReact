37a130e3409f010b073460b582c63ad1d3d37e93
victornavareno <vnavareo@alumnos.unex.es>
2024-11-18 14:58:59 +0100

    reproductor funciona

--- src/components/Reproductor.jsx
+++ src/components/Reproductor.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "../styles/Reproductor.css";
-import Contenido from "./ContenidoDetail";
 
-function Reproductor({ contenido }) {
+function Reproductor({ contenido, onClose }) {
   return (
     <div className="reproductor">
       <div className="reproductor-thumbnail">
@@ -10,12 +9,17 @@ function Reproductor({ contenido }) {
           src={`/assets/img/${contenido.imagen}`}
           alt={contenido.imagen}
         />
-        {/* Play button on top */}
+        {/* Botón de reproducción */}
         <button className="play-button">
-                ▶
-          <i className="fas fa-play"></i> {/* FontAwesome play icon */}
+          ▶ <i className="fas fa-play"></i> {/* Icono de reproducción */}
         </button>
       </div>
+
+      {/* Botón para salir del reproductor */}
+      <button className="close-button" onClick={onClose}>
+        {/* ✖️ Salir */}
+        Volver
+      </button>
     </div>
   );
 }
