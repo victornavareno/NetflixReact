833e303f386fedb29f994c9543234695d0abadf1
Soledad <mdiaznc@alumnos.unex.es>
2024-11-24 19:45:47 +0100

    Botón Crear Nueva Vista

--- src/App.js
+++ src/App.js
@@ -98,17 +98,27 @@ function App() {
             <Routes>
               <Route
                 path="/"
-                element={vistas
-                  .map((vista) => (
+                element={
+                  <>
+                  {usuarioSeleccionado && usuarioSeleccionado.rol === 'administrador' && (
+                    <div className="botonNuevaVista" 
+                      //  onClick={() => setMostrarFormulario(true)}
+                    >
+                      <p>Crear Nueva Vista</p>
+                    </div>
+                  )}
+                  {vistas.map((vista) => (
                     <Vista key={vista.id_vista} vista={vista} />
                   ))}
-              />
-              <Route path="/contenido/:id" element={<ContenidoDetail setUsuarioSeleccionado={setUsuarioSeleccionado} />} />{" "}
-              {/* PAGINA DE DETALLE DE CONTENIDO */}
-              <Route path="/contenido" element={<ContenidoList />} />{" "}
-              {/* PAGINA DE LISTA DE CONTENIDOS */}
-              <Route path="/login" element={<Usuarios setUsuarioSeleccionado={setUsuarioSeleccionado} />} />{" "}
-              {/* PAGINA DE LOGIN */}
+                </>
+              }
+            />
+            <Route path="/contenido/:id" element={<ContenidoDetail setUsuarioSeleccionado={setUsuarioSeleccionado} />} />{" "}
+            {/* PAGINA DE DETALLE DE CONTENIDO */}
+            <Route path="/contenido" element={<ContenidoList />} />{" "}
+            {/* PAGINA DE LISTA DE CONTENIDOS */}
+            <Route path="/login" element={<Usuarios setUsuarioSeleccionado={setUsuarioSeleccionado} />} />{" "}
+            {/* PAGINA DE LOGIN */}
             </Routes>
           </main>
         </div>
